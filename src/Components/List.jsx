a71d5d51d5351f674c113e8b4e294b48f9768cf8
bee <beebelam0@gmail.com>
2023-10-02 11:39:52 +0300

    third push

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -4,15 +4,25 @@ function List() {
     const [tours] = useState(data)
     const[readMore,setReadMore] = useState(false)
   return (
-    <div>
+    <div className='tainer'>
+        <div>
+        </div>
+    <div className='box'>
+    <h2>Our Tours</h2>
         {tours.map((tour)=> (
-            <div key={tour.id}>
+            <div key={tour.id} >
+                <div className='con'>
                 <img src={tour.image} alt="not found" />
                 <h1>{tour.title}</h1>
               {readMore ? tour.des : `${tour.des.substring(0,200)}...`}
-              <button onClick={()=> setReadMore(!readMore)}>{readMore ? 'Show Less' : 'Show more'}</button>
+              <span onClick={()=> setReadMore(!readMore)}>{readMore ? 'Show Less' : 'Show more'}</span>
+              <div className='but'> 
+              <button>Not Interested</button>
+              </div>
+            </div>
             </div>
         ))}
+    </div>
     </div>
   )
 }
