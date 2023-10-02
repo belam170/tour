2a7c0bdbdbd865dc2c7639da8af3ab4f9eb1494e
bee <beebelam0@gmail.com>
2023-10-02 17:44:19 +0300

    final commit

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
      
-      <List  />
+      <List   />
     </div>
   )
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from 'react'
-import data from './Data'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
+import data from './Data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function List() {
-    const [tours,setTours] = useState(data)
-    const[readMore,setReadMore] = useState(false)
+  const [tours, setTours] = useState(data);
+  const [readMore, setReadMore] = useState(false);
 
+  const handleDelete = (id) => {
+    setTours((oldTours) => {
+      return oldTours.filter((tour) => tour.id !== id);
+    });
+  };
 
-    const handleDelete = (id) => {
-        setTours(oldTours => {
-            return oldTours.filter(tour => tour.id !== id)
-        })
-    }
   return (
     <div className='tainer'>
-    <div className='box'>
-    <h2>Our Tours</h2>
-    {tours.length === 0 && <div><FontAwesomeIcon icon={faSearch} />Nothing to display</div>}
-        {tours.map((tour)=> (
-            <div key={tour.id} >
-                <div className='con'>
-                <img src={tour.image} alt="not found" />
+      <div className='box'>
+        <h2>Our Tours</h2>
+        {tours.length === 0 ? (
+          <div className='no-tours-message'>
+            <div className='no-tours-icon'>
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <p className='no-tours-text'>Nothing to display</p>
+          </div>
+        ) : (
+          tours.map((tour) => (
+            <div key={tour.id}>
+              <div className='con'>
+                <img src={tour.image} alt='not found' />
                 <h1>{tour.title}</h1>
-              {readMore ? tour.des : `${tour.des.substring(0,200)}...`}
-              <span onClick={()=> setReadMore(!readMore)}>{readMore ? 'Show Less' : 'Show more'}</span>
-              <div className='but'> 
-              <button onClick={()=> handleDelete(tour.id)}>Not Interested</button>
+                <p>{readMore ? tour.des : `${tour.des.substring(0, 200)}...`}
+                <span onClick={() => setReadMore(!readMore)}>
+                  {readMore ? 'Show Less' : 'Show more'}
+                </span>
+                </p>
+                <div className='but'>
+                  <button onClick={() => handleDelete(tour.id)}>
+                    Not Interested
+                  </button>
+                  <div className='price-circle'>
+                ${tour.price} 
+              </div>
+                </div>
               </div>
             </div>
-            </div>
-        ))}
+          ))
+        )}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default List
+export default List;
